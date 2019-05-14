bf28123f7a3e91008bb53195ed6ddf0ff7e3559d
buketkonuk <bukikir@gmail.com>
2019-05-14 11:13:12 +0100

    change message

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div>
       <h2>Hello Kodiri bootCamp!</h2>
+      <h3>This is the travis CI demo</h3>
         {productsData.map(product => 
           <div>
           <p>product Name: {product.name}</p>
