23377db4b5c2388aaae347c47a3a91d79efdfa1b
Graham Hewett <hewettgraham@gmail.com>
2019-05-16 14:15:26 +0100

    add friday change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <h2>Hello Kodiri bootCamp!</h2>
-      <h3>Testing from Gray's laptop!</h3>
+      <h3>Late friday change oh yeah</h3>
         {productsData.map(product => 
           <div>
           <p>product Name: {product.name}</p>
